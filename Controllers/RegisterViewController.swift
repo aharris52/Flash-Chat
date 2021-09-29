238f68086d84f29a61b0bfb68be09d8f5b3ff139
Andrew Harris <59749069+aharris52@users.noreply.github.com>
2021-09-29 16:58:04 -0700

    Login/logout supported. Removed back button from chat VC.

--- Controllers/RegisterViewController.swift
+++ Controllers/RegisterViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            // Register new email/password
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                // Check for errors
+                if let e = error {
+                    print(e)
+                // if successful segue to the chat screen
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
-    
 }
